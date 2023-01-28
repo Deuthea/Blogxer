94e4bffb46963d7982f15ac4c6e6b57eeff98ad3
Tarun Choudhary <tarunchoudhary1805@gmail.com>
2023-01-28 22:35:01 +0530

    commit

--- src/Components/Blogs/Blog.jsx
+++ src/Components/Blogs/Blog.jsx
@@ -1,9 +1,101 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import "./Blog.css";
 
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <>
+      <Navbar />
+      <div className="container">
+        <div class=" mb-3 mt-2 d-flex flex-column justify-start align-items-center border-bottom col-lg-12">
+          <div class=" row g-0 mb-3 ">
+            <div class="px-0 col-md-12 ">
+              <div class="card-body">
+                <p className="d-flex justify-content-between">
+                  <p className="d-flex align-items-center ">
+                    <span class="material-symbols-outlined profile-blog pr-2">
+                      <img
+                        style={{
+                          height: "25px",
+                          width: "25px",
+                          objectFit: "cover",
+                        }}
+                        src="https://source.unsplash.com/random"
+                        class="  rounded-pill "
+                        alt="..."
+                      />
+                    </span>
+                    <span
+                      class="card-tile font-weight-bold pt-2"
+                      style={{ fontSize: "14px" }}
+                    >
+                      Tarun Choudhary
+                    </span>
+                    <span className="pl-2 pt-2">·</span>
+                    <span
+                      class="pt-2 pl-2"
+                      style={{
+                        fontSize: "13px",
+                        fontWeight: "400",
+                        color: "#333",
+                      }}
+                    >
+                      Feb 7, 2016
+                    </span>
+                    <span className="pl-2 pt-2">·</span>
+                    <span
+                      class="pt-2 px-2"
+                      style={{
+                        fontSize: "13px",
+                        fontWeight: "400",
+                        color: "#333",
+                      }}
+                    >
+                      6 min read
+                    </span>
+                  </p>
+                  <p>
+                    <span class="material-symbols-outlined">bookmark_add</span>
+                  </p>
+                </p>
+                <h3 class="card-title font-weight-bold">
+                  Card with an image on left
+                </h3>
+                <p class="card-text description">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer. This is a wider card with supporting text below as a
+                  natural lead-in to additional content. This content is a
+                  little bit longer. This is a wider card with supporting text
+                  below as a natural lead-in to additional content. This content
+                  is a little bit longer. This is a wider card with supporting
+                  text below as a natural lead-in to additional content. This
+                  content is a little bit longer. This is a wider card with
+                  supporting text below as a natural lead-in to additional
+                  content. This content is a little bit longer.This is a wider
+                  card with supporting text below as a natural lead-in to
+                  additional content. This content is a little bit longer.This
+                  is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.This is a wider card with supporting text below as a
+                  natural lead-in to additional content. This content is a
+                  little bit longer.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="px-0 py-1 col-md-10 ">
+            <img
+              style={{ height: "80vh", width: "60vw", objectFit: "contain" }}
+              src="https://source.unsplash.com/random"
+              class=" img-fluid  rounded "
+              alt="..."
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Blog
+export default Blog;
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,272 +1,99 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 
 const Blogs = () => {
   return (
     <div>
-      <div class=" mb-3 mt-2 border-bottom">
-        <div class=" row g-0 mb-3">
-          <div class="px-0 col-md-9">
-            <div class="card-body">
-              <p className="d-flex align-items-center ">
-                <span class="material-symbols-outlined profile-blog pr-2">
-                  <img
-                    style={{
-                      height: "25px",
-                      width: "25px",
-                      objectFit: "cover",
-                    }}
-                    src="https://source.unsplash.com/random"
-                    class="  rounded-pill "
-                    alt="..."
-                  />
-                </span>
-                <span
-                  class="card-tile font-weight-bold pt-2"
-                  style={{ fontSize: "14px" }}
-                >
-                  Tarun Choudhary
-                </span>
-                <span className="pl-2 pt-2">·</span>
-                <span
-                  class="pt-2 px-2"
-                  style={{
-                    fontSize: "13px",
-                    fontWeight: "400",
-                    color: "#333",
-                  }}
-                >
-                  Feb 7, 2016
-                </span>
-              </p>
-              <h5 class="card-title font-weight-bold">
-                Card with an image on left
-              </h5>
-              <p class="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-              <p className="d-flex justify-content-between">
-                <p>
+      <Link className="text-dark text-decoration-none" to="/blog">
+        <div class=" mb-3 mt-2 border-bottom">
+          <div class=" row g-0 mb-3">
+            <div class="px-0 col-md-9">
+              <div class="card-body">
+                <p className="d-flex align-items-center ">
+                  <span class="material-symbols-outlined profile-blog pr-2">
+                    <img
+                      style={{
+                        height: "25px",
+                        width: "25px",
+                        objectFit: "cover",
+                      }}
+                      src="https://source.unsplash.com/random"
+                      class="  rounded-pill "
+                      alt="..."
+                    />
+                  </span>
                   <span
-                    class="rounded-pill"
+                    class="card-tile font-weight-bold pt-2"
+                    style={{ fontSize: "14px" }}
+                  >
+                    Tarun Choudhary
+                  </span>
+                  <span className="pl-2 pt-2">·</span>
+                  <span
+                    class="pt-2 px-2"
                     style={{
                       fontSize: "13px",
-                      background: "#f0f0f0",
-                      padding: "5px 8px",
                       fontWeight: "400",
-                      color: "#000",
+                      color: "#333",
                     }}
                   >
-                    Artificial Intellijence
-                  </span>
-                  <span className="pl-2">·</span>
-                  <span
-                    class=""
-                    style={{
-                      fontSize: "13px",
-                      // background: "#f0f0f0",
-                      padding: "5px 8px",
-                      fontWeight: "400",
-                      color: "#666",
-                    }}
-                  >
-                    6 min read
+                    Feb 7, 2016
                   </span>
                 </p>
-                <p>
-                  <span class="material-symbols-outlined">bookmark_add</span>
+                <h5 class="card-title font-weight-bold">
+                  Card with an image on left
+                </h5>
+                <p class="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
                 </p>
-              </p>
+                <p className="d-flex justify-content-between">
+                  <p>
+                    <span
+                      class="rounded-pill"
+                      style={{
+                        fontSize: "13px",
+                        background: "#f0f0f0",
+                        padding: "5px 8px",
+                        fontWeight: "400",
+                        color: "#000",
+                      }}
+                    >
+                      Artificial Intellijence
+                    </span>
+                    <span className="pl-2">·</span>
+                    <span
+                      class=""
+                      style={{
+                        fontSize: "13px",
+                        // background: "#f0f0f0",
+                        padding: "5px 8px",
+                        fontWeight: "400",
+                        color: "#666",
+                      }}
+                    >
+                      6 min read
+                    </span>
+                  </p>
+                  <p>
+                    <span class="material-symbols-outlined">bookmark_add</span>
+                  </p>
+                </p>
+              </div>
+            </div>
+            <div class="px-4 py-5 col-md-3 ">
+              <img
+                style={{ height: "20vh", width: "20vw", objectFit: "cover" }}
+                src="https://source.unsplash.com/random"
+                class=" img-fluid  rounded "
+                alt="..."
+              />
             </div>
           </div>
-          <div class="px-4 py-5 col-md-3 ">
-            <img
-              style={{ height: "20vh", width: "20vw", objectFit: "cover" }}
-              src="https://source.unsplash.com/random"
-              class=" img-fluid  rounded "
-              alt="..."
-            />
-          </div>
         </div>
-      </div>
-
-      <div class=" mb-3 mt-2 border-bottom">
-        <div class=" row g-0 mb-3">
-          <div class="px-0 col-md-9">
-            <div class="card-body">
-              <p className="d-flex align-items-center ">
-                <span class="material-symbols-outlined profile-blog pr-2">
-                  <img
-                    style={{
-                      height: "25px",
-                      width: "25px",
-                      objectFit: "cover",
-                    }}
-                    src="https://source.unsplash.com/random"
-                    class="  rounded-pill "
-                    alt="..."
-                  />
-                </span>
-                <span
-                  class="card-tile font-weight-bold pt-2"
-                  style={{ fontSize: "14px" }}
-                >
-                  Tarun Choudhary
-                </span>
-                <span className="pl-2 pt-2">·</span>
-                <span
-                  class="pt-2 px-2"
-                  style={{
-                    fontSize: "13px",
-                    fontWeight: "400",
-                    color: "#333",
-                  }}
-                >
-                  Feb 7, 2016
-                </span>
-              </p>
-              <h5 class="card-title font-weight-bold">
-                Card with an image on left
-              </h5>
-              <p class="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-              <p className="d-flex justify-content-between">
-                <p>
-                  <span
-                    class="rounded-pill"
-                    style={{
-                      fontSize: "13px",
-                      background: "#f0f0f0",
-                      padding: "5px 8px",
-                      fontWeight: "400",
-                      color: "#000",
-                    }}
-                  >
-                    Artificial Intellijence
-                  </span>
-                  <span className="pl-2">·</span>
-                  <span
-                    class=""
-                    style={{
-                      fontSize: "13px",
-                      // background: "#f0f0f0",
-                      padding: "5px 8px",
-                      fontWeight: "400",
-                      color: "#666",
-                    }}
-                  >
-                    6 min read
-                  </span>
-                </p>
-                <p>
-                  <span class="material-symbols-outlined">bookmark_add</span>
-                </p>
-              </p>
-            </div>
-          </div>
-          <div class="px-4 py-5 col-md-3 ">
-            <img
-              style={{ height: "20vh", width: "20vw", objectFit: "cover" }}
-              src="https://source.unsplash.com/random"
-              class=" img-fluid  rounded "
-              alt="..."
-            />
-          </div>
-        </div>
-      </div>
-
-      <div class=" mb-3 mt-2 border-bottom">
-        <div class=" row g-0 mb-3">
-          <div class="px-0 col-md-9">
-            <div class="card-body">
-              <p className="d-flex align-items-center ">
-                <span class="material-symbols-outlined profile-blog pr-2">
-                  <img
-                    style={{
-                      height: "25px",
-                      width: "25px",
-                      objectFit: "cover",
-                    }}
-                    src="https://source.unsplash.com/random"
-                    class="  rounded-pill "
-                    alt="..."
-                  />
-                </span>
-                <span
-                  class="card-tile font-weight-bold pt-2"
-                  style={{ fontSize: "14px" }}
-                >
-                  Tarun Choudhary
-                </span>
-                <span className="pl-2 pt-2">·</span>
-                <span
-                  class="pt-2 px-2"
-                  style={{
-                    fontSize: "13px",
-                    fontWeight: "400",
-                    color: "#333",
-                  }}
-                >
-                  Feb 7, 2016
-                </span>
-              </p>
-              <h5 class="card-title font-weight-bold">
-                Card with an image on left
-              </h5>
-              <p class="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-              <p className="d-flex justify-content-between">
-                <p>
-                  <span
-                    class="rounded-pill"
-                    style={{
-                      fontSize: "13px",
-                      background: "#f0f0f0",
-                      padding: "5px 8px",
-                      fontWeight: "400",
-                      color: "#000",
-                    }}
-                  >
-                    Artificial Intellijence
-                  </span>
-                  <span className="pl-2">·</span>
-                  <span
-                    class=""
-                    style={{
-                      fontSize: "13px",
-                      // background: "#f0f0f0",
-                      padding: "5px 8px",
-                      fontWeight: "400",
-                      color: "#666",
-                    }}
-                  >
-                    6 min read
-                  </span>
-                </p>
-                <p>
-                  <span class="material-symbols-outlined">bookmark_add</span>
-                </p>
-              </p>
-            </div>
-          </div>
-          <div class="px-4 py-5 col-md-3 ">
-            <img
-              style={{ height: "20vh", width: "20vw", objectFit: "cover" }}
-              src="https://source.unsplash.com/random"
-              class=" img-fluid  rounded "
-              alt="..."
-            />
-          </div>
-        </div>
-      </div>
+      </Link>
     </div>
   );
 };
