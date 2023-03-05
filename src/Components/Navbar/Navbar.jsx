06f556fc7dd0112422640fb61da27f7325340922
Tarun Choudhary <tarunchoudhary1805@gmail.com>
2023-03-05 18:43:23 +0530

    Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -110,6 +110,7 @@ const Navbar = ({ page }) => {
                   >
                     <span class="sr-only">Open user menu</span>
                     <img
+                      onClick={() => setMode(!mode)}
                       class="h-8 w-8 rounded-full"
                       src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                       alt=""
@@ -118,39 +119,75 @@ const Navbar = ({ page }) => {
                 </div>
 
                 <div
-                  class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  className={`absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${
+                    mode ? "block" : "hidden"
+                  }`}
                   role="menu"
                   aria-orientation="vertical"
                   aria-labelledby="user-menu-button"
                   tabindex="-1"
                 >
-                  <a
-                    href="#"
-                    class="block px-4 py-2 text-sm text-gray-700"
+                  <a class="flex align-middle justify-center pb-2 px-4 font-bold text-md text-gray-700">
+                    {state.name}
+                  </a>
+                  <hr className="border-b-2 mx-3" />
+                  <Link
+                    to="/profile"
+                    class="flex align-middle justify-center  px-3 py-2 text-sm text-gray-700"
                     role="menuitem"
                     tabindex="-1"
                     id="user-menu-item-0"
                   >
-                    Your Profile
-                  </a>
+                    <span className="mr-1">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        className="w-5 h-5"
+                      >
+                        <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
+                      </svg>
+                    </span>
+
+                    <span className="">Your Profile</span>
+                  </Link>
+                  <hr className="border-b-1 mx-3" />
+
                   <a
-                    href="#"
-                    class="block px-4 py-2 text-sm text-gray-700"
-                    role="menuitem"
-                    tabindex="-1"
-                    id="user-menu-item-1"
-                  >
-                    Settings
-                  </a>
-                  <a
-                    href="#"
-                    class="block px-4 py-2 text-sm text-gray-700"
+                    onClick={() => {
+                      dispatch(logout());
+                      toast.warn("Logout Successfull ✅", {
+                        position: toast.POSITION.TOP_CENTER,
+                      });
+                    }}
+                    // href="#"
+                    class="flex cursor-pointer justify-center px-4 py-2 text-sm text-gray-700"
                     role="menuitem"
                     tabindex="-1"
                     id="user-menu-item-2"
                   >
-                    Sign out
+                    <span className="mr-1">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        class="w-5 h-5"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M3 4.25A2.25 2.25 0 015.25 2h5.5A2.25 2.25 0 0113 4.25v2a.75.75 0 01-1.5 0v-2a.75.75 0 00-.75-.75h-5.5a.75.75 0 00-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 00.75-.75v-2a.75.75 0 011.5 0v2A2.25 2.25 0 0110.75 18h-5.5A2.25 2.25 0 013 15.75V4.25z"
+                          clip-rule="evenodd"
+                        />
+                        <path
+                          fill-rule="evenodd"
+                          d="M19 10a.75.75 0 00-.75-.75H8.704l1.048-.943a.75.75 0 10-1.004-1.114l-2.5 2.25a.75.75 0 000 1.114l2.5 2.25a.75.75 0 101.004-1.114l-1.048-.943h9.546A.75.75 0 0019 10z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </span>
+                    <span> Sign out</span>
                   </a>
+                  {/* <hr className="border-b-2 mx-3" /> */}
                 </div>
               </div>
             </div>
